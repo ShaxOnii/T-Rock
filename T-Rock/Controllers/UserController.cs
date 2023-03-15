094c5586c98a67532bedbf5f492fd1961f233481
ikochacRito <ikochacRito@DESKTOP-605AC1P>
2023-03-15 20:45:55 +0100

    Dodaj pliki projektów.

---
+++ T-Rock/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace T_Rock.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserName()
+        {
+            return View();
+        }
+    }
+}
