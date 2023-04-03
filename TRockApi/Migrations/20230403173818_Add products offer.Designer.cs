963a22f3237fbb75d316f6c27b25bf01759a96d1
Piotr Sniezek <sniegu703@gmail.com>
2023-04-03 19:53:19 +0200

    Add product catalog migrations

--- TRockApi/Migrations/20230403173818_Add products offer.Designer.cs
+++ TRockApi/Migrations/20230403173818_Add products offer.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TRockApi.Repositories.Configuration;
 
@@ -10,9 +11,11 @@ using TRockApi.Repositories.Configuration;
 namespace TRockApi.Migrations
 {
     [DbContext(typeof(ShopDbContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20230403173818_Add products offer")]
+    partial class Addproductsoffer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
