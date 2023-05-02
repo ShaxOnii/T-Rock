06c9883dc1cd6dea8e21ce3f8901d20495ad9c27
Piotr Sniezek <sniegu703@gmail.com>
2023-05-02 11:05:51 +0200

    Add unit testing project

---
+++ TRockTest/Carts/AddCartItemsTest.cs
@@ -0,0 +1,23 @@
+namespace TRockTest.Carts;
+
+public class AddCartItemsTest {
+    
+    [SetUp]
+    public void Setup() { }
+
+    
+    [Test]
+    public void AddSingleItemToNonExistingCart() {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void AddSingleItemToExistingCart() {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void AddManyItemsToCart() {
+        Assert.Pass();
+    }
+}
